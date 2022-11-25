01c41ede90bc37d1369a1e9b7b2005a73c625f43
Sanket Kumbhar <ksanket114402@gmail.com>
2022-11-25 18:48:17 +0530

    added new features

--- Frontend/src/context/Context.js
+++ Frontend/src/context/Context.js
@@ -1,4 +1,5 @@
 import { createContext,useReducer } from "react";
+import Reducer from "./Reducer";
 
 const INITIAL_STATE = {
     user : null,
@@ -9,5 +10,13 @@ const INITIAL_STATE = {
 export const Context = createContext();
 
 export const ContextProvider = ({children}) =>{
+    const [state,dispatch] = useReducer(Reducer,INITIAL_STATE);
     
+    return <Context.Provider
+    value={{
+        user:state.user,
+        isFetching:state.isFetching,
+        error : state.error,
+    }}>
+        {children}</Context.Provider>
 }
